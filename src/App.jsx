3bf6a8edd30843bd60aa8d4e5a1a0c2d208f3162
Artem Trifonov <artemis.levitt@gmail.com>
2025-03-12 14:22:51 +0100

    Fetch questions from fake API server

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
+import {useEffect} from "react";
 
 
 function App() {
-  return (
+    useEffect(() => {
+        fetch("http://localhost:8000/questions").then((res) =>
+            res.json()).then(data=>console.log(data)).catch(err=>console.error(err));
+    }, []);
+
+    return (
       <div className="app">
         <Header />
 
@@ -12,7 +18,7 @@ function App() {
               <p>Question?</p>
           </Main>
       </div>
-  );
+    );
 }
 
 export default App
