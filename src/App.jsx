c54a0c9742c145eb8006c9be31cb366f8d24a697
Artem Trifonov <artemis.levitt@gmail.com>
2025-03-12 14:02:49 +0100

    Setup Header and Main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import DateCounter from "./components/DateCounter.jsx";
+import Header from "./components/Header.jsx";
+import Main from "./components/Main.jsx";
 
 
 function App() {
   return (
-      <div>
-          <DateCounter />
+      <div className="app">
+        <Header />
+
+          <Main>
+              <p>1/15</p>
+              <p>Question?</p>
+          </Main>
       </div>
   );
 }
