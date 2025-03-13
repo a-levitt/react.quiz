9926fb56483f8ff5073780fbcee2fa13412e3b6c
Artem Trifonov <artemis.levitt@gmail.com>
2025-03-13 17:21:09 +0100

    Handle new answers

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ import Question from "./components/Question.jsx";
 const initialState = {
     questions: [],
     status: 'loading',
-    index: 0
+    index: 0,
+    answer: null,
+    points: 0
 }
 
 function reducer(state, action) {
@@ -30,13 +32,24 @@ function reducer(state, action) {
                 ...state,
                 status: 'active'
             }
+        case 'newAnswer':
+            {
+                const question = state.questions.at(state.index);
+                return {
+                    ...state,
+                    answer: action.payload,
+                    points: action.payload === question.correctOption ?
+                        state.points + question.points
+                        : state.points
+                }
+            }
         default:
             throw new Error(`Unknown action type ${action.type}`);
     }
 }
 
 function App() {
-    const [{questions, status, index}, dispatch] = useReducer(reducer, initialState);
+    const [{questions, status, index, answer}, dispatch] = useReducer(reducer, initialState);
     const numQuestions = questions.length;
 
     useEffect(() => {
@@ -53,7 +66,7 @@ function App() {
               {status === 'loading' && <Loader/>}
               {status === 'error' && <Error/>}
               {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch} />}
-              {status === 'active' && <Question question={questions[index]}/>}
+              {status === 'active' && <Question question={questions[index]} dispatch={dispatch} answer={answer} />}
           </Main>
       </div>
     );
