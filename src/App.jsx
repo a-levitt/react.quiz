fe76e350e6bf9200c32a9315c4b148f85be07768
Artem Trifonov <artemis.levitt@gmail.com>
2025-03-18 15:16:45 +0100

    Add Timer component

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Question from "./components/Question.jsx";
 import NextButton from "./components/NextButton.jsx";
 import Progress from "./components/Progress.jsx";
 import FinishScreen from "./components/FinishScreen.jsx";
+import Timer from "./components/Timer.jsx";
+import Footer from "./components/Footer.jsx";
+
+const SECS_PER_QUESTION = 30;
 
 const initialState = {
     questions: [],
@@ -15,7 +19,8 @@ const initialState = {
     index: 0,
     answer: null,
     points: 0,
-    highscore: 0
+    highscore: 0,
+    secondsRemaining: null
 }
 
 function reducer(state, action) {
@@ -34,7 +39,8 @@ function reducer(state, action) {
         case 'start':
             return {
                 ...state,
-                status: 'active'
+                status: 'active',
+                secondsRemaining: state.questions.length * SECS_PER_QUESTION
             }
         case 'newAnswer':
             {
@@ -68,6 +74,14 @@ function reducer(state, action) {
                 highscore: state.highscore,
                 status: 'active'
             }
+        case 'tick':
+            return {
+                ...state,
+                secondsRemaining: state.secondsRemaining - 1,
+                status: state.secondsRemaining === 0 ?
+                    'finished'
+                    : state.status
+            }
 
         default:
             throw new Error(`Unknown action type ${action.type}`);
@@ -75,7 +89,7 @@ function reducer(state, action) {
 }
 
 function App() {
-    const [{questions, status, index, answer, points, highscore}, dispatch] = useReducer(reducer, initialState);
+    const [{questions, status, index, answer, points, highscore, secondsRemaining}, dispatch] = useReducer(reducer, initialState);
     const numQuestions = questions.length;
     const maxPoints = questions.reduce((prev, cur) => prev + cur.points, 0)
 
@@ -107,12 +121,18 @@ function App() {
                         dispatch={dispatch}
                         answer={answer}
                       />
-                      <NextButton
-                          dispatch={dispatch}
-                          answer={answer}
-                          index={index}
-                          numQuestions={numQuestions}
-                      />
+                      <Footer>
+                          <Timer
+                              dispatch={dispatch}
+                              secondsRemaining={secondsRemaining}
+                          />
+                          <NextButton
+                              dispatch={dispatch}
+                              answer={answer}
+                              index={index}
+                              numQuestions={numQuestions}
+                          />
+                      </Footer>
                   </>
               }
               {status === 'finished' &&
